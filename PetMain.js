7c584824182671b30cb39185a9fc7fcc1ae8d4d2
leigharobinson <lmarobinson24@gmail.com>
2020-04-22 15:45:36 -0500

    I created an object that represented my hypothetical pet

---
+++ PetMain.js
@@ -0,0 +1,21 @@
+console.log("We can do anything we want to do if we stick to it long enough -Hellen Keller ");
+
+// Create an object that represents your pet.
+
+// Name property with a string value.
+// Species property with a string value.
+// Nicknames property with an array value. Array contains strings.
+// Age property with an integer value
+
+let myPet = {
+    name: "Sherpa",
+    species: "dog",
+    nicknames: ["big Floofer", "doggo", "polar bear", "Wooyoo", "best boi", "goodest boi", "thicc boi", "stinkers", "pumpkin", "D'aww", "shweetness"],
+    age: 2
+}
+
+
+for (let i = 0; i < myPet.nicknames.length; i++) {
+    console.log(myPet.nicknames[i]);
+
+}
